8f50ce06cb365a8e511f2d0783549c3e8166cbf6
陈进煌 <2219316464@qq.com>
2023-02-22 15:07:31 +0800

    simple factory test

--- 00_simple_factory/simple_test.go
+++ 00_simple_factory/simple_test.go
@@ -9,3 +9,11 @@ func TestType1(t *testing.T) {
 		t.Fatal("Type1 test fail")
 	}
 }
+
+func TestType2(t *testing.T) {
+	api := NewAPI(2)
+	s := api.Say("Tom")
+	if s != "Hello, Tom" {
+		t.Fatal("Type1 test fail")
+	}
+}
